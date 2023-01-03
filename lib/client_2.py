475dcf4808b0e0d3e9f2d36ada14a302d04e409d
Griphu <kzielinski2727@gmail.com>
2023-01-03 22:06:43 +0100

    comparison of the datasets

---
+++ lib/client_2.py
@@ -0,0 +1,108 @@
+import warnings
+from collections import OrderedDict
+
+import flwr as fl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import Compose, Normalize, ToTensor
+from tqdm import tqdm
+from pathlib import Path
+from torchvision.datasets import ImageFolder
+
+
+# #############################################################################
+# 1. Regular PyTorch pipeline: nn.Module, train, test, and DataLoader
+# #############################################################################
+
+warnings.filterwarnings("ignore", category=UserWarning)
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
+model.classifier = nn.Sequential(nn.Linear(in_features = 1280,out_features = 640),
+                                 nn.ReLU(inplace = True),
+                                 nn.Linear(in_features = 640,out_features = 3))
+for param in model.parameters():
+    param.requires_grad = True
+
+def train(net, trainloader, epochs):
+    """Train the model on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for _ in range(epochs):
+        for images, labels in tqdm(trainloader):
+            optimizer.zero_grad()
+            criterion(net(images.to(DEVICE)), labels.to(DEVICE)).backward()
+            optimizer.step()
+
+
+def test(net, testloader):
+    """Validate the model on the test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(testloader):
+            outputs = net(images.to(DEVICE))
+            labels = labels.to(DEVICE)
+            loss += criterion(outputs, labels).item()
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    return loss / len(testloader.dataset), correct / total
+
+
+def load_data():
+    """Load MIAS (training and test set)."""
+    norm = [0.5, 0.5, 0.5]
+    train_transform = transforms.Compose([transforms.Resize((250, 250)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(norm, norm)])
+
+    test_transform = transforms.Compose([transforms.Resize((250, 250)),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(norm, norm)])
+
+    train_data = ImageFolder("/mias/train", transform=train_transform)
+    test_data = ImageFolder("/mias/test", transform=test_transform)
+
+    return DataLoader(train_data, batch_size=32, shuffle=True), DataLoader(test_data)
+
+
+# #############################################################################
+# 2. Federation of the pipeline with Flower
+# #############################################################################
+
+# Load model and data (simple CNN, CIFAR-10)
+net = model.to(DEVICE)
+trainloader, testloader = load_data()
+
+# Define Flower client
+class FlowerClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(net, trainloader, epochs=1)
+        return self.get_parameters(config={}), len(trainloader.dataset), {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(net, testloader)
+        return loss, len(testloader.dataset), {"accuracy": accuracy}
+
+
+# Start Flower client
+fl.client.start_numpy_client(
+    server_address="153.19.51.95:4466",
+    client=FlowerClient(),
+    root_certificates=Path("/media/magda/DYSKWIN/dyskD/federated/kody/certificates/cache/certificates/ca.crt").read_bytes(),
+)
